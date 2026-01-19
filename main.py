64615c1ebe4265e5cd91a675316b69e906ad4166
aLadNamedPat <Pattheboy135@gmail.com>
2026-01-19 18:44:38 -0500

    added

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ Env = gym.make("CarRacing-v3")
 training = Train(Env)
 training.initialize(3, 3, 32, [64, 64, 128, 128], 3, 35, 5, hidden_layer=256)
 
-# for i in range(1000):
-#     print(i)
-#     training.rollout(random_action=True, save_images=True, save_root= "rollouts", max_steps = 1000)
+for i in range(10000):
+    print(i)
+    training.rollout(random_action=True, save_images=True, save_root= "rollouts", max_steps = 1000)
 
 training.VAE_Train(5)
